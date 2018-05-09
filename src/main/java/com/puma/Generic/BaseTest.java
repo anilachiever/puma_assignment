f612c9b6a1cd9a416155091bb9d1eac51f7b6f3f
tyss <tyss@198.105.254.11>
2018-05-09 11:08:57 +0530

    puma_assign

---
+++ src/main/java/com/puma/Generic/BaseTest.java
@@ -0,0 +1,29 @@
+package com.puma.Generic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest implements IAutoconst {
+	public WebDriver driver;
+	
+	@BeforeMethod
+	public void openApplication()
+	{
+		System.setProperty(CHROME_KEY, CHROME_VALUE);
+		driver=new ChromeDriver();
+		driver.get("https://in.puma.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	}
+
+	
+	@AfterMethod
+	public void closeApplication()
+	{
+		driver.quit();
+	}
+}
---
+++ src/main/java/com/puma/Generic/IAutoconst.java
@@ -0,0 +1,15 @@
+package com.puma.Generic;
+
+public interface IAutoconst {
+	
+	
+		//Storing Key-Value pair for geckoDriver and ChromeDriver
+		String GECKO_KEY="webdriver.gecko.driver";
+		String GECKO_VALUE="./driver/geckodriver";
+		
+		String CHROME_KEY="webdriver.chrome.driver";
+		String CHROME_VALUE="./driver/chromedriver";
+	
+
+
+}
